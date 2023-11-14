c7c4571a6e59b67ac1fb22b5afce07e1a3812a6b
lemon-lime-honey <bs0308@g.skku.edu>
2023-11-14 19:45:24 +0900

    test: order input test
    - 주문 입력 테스트 추가

---
+++ src/test/java/christmas/OrderInputTest.java
@@ -0,0 +1,60 @@
+package christmas;
+
+import christmas.userinput.OrderInput;
+import christmas.order.Menu;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class OrderInputTest extends NsTest {
+    private static OrderInput orderInput;
+    private static final String stop = "샴페인-1";
+
+    @DisplayName("주문 입력: -로 메뉴와 수량을 구분하지 않았을 때")
+    @Test
+    void nonDashOrder() {
+        String input = "양송이수프1";
+
+        assertSimpleTest(() -> {
+            run(input, stop);
+            assertThat(output())
+                .contains("[ERROR]");
+        });
+    }
+
+    @DisplayName("주문 입력: ,로 각 세트를 구분하지 않았을 때")
+    @Test
+    void nonCommaOrder() {
+        String input = "타파스-2 시저샐러드-3";
+
+        assertSimpleTest(() -> {
+            run(input, stop);
+            assertThat(output())
+                .contains("[ERROR]");
+        });
+    }
+
+    @DisplayName("주문 입력: 수량이 0일 때")
+    @Test
+    void zeroQuantity() {
+        String input = "티본스테이크-0,바비큐립-1";
+
+        assertSimpleTest(() -> {
+            run(input, stop);
+            assertThat(output())
+                .contains("[ERROR]");
+        });
+    }
+
+
+    @Override
+    public void runMain() {
+        orderInput = new OrderInput();
+        orderInput.order();
+    }
+}
