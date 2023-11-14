c42d5b259d1693f8f9f39da031a3f3b00788a26c
lemon-lime-honey <bs0308@g.skku.edu>
2023-11-14 16:23:32 +0900

    refactor: date input
    - 날짜를 별개의 클래스로 분리

--- src/main/java/christmas/userinput/Input.java
+++ src/main/java/christmas/userinput/Input.java
@@ -1,35 +1,10 @@
 package christmas.userinput;
 
-import camp.nextstep.edu.missionutils.Console;
-
 public class Input {
+    private Date dateInput = new Date();
+
     public void run() {
         System.out.println(Message.INIT.getMessage());
-        int date = loopDateInput();
-    }
-
-    private int runDateInput() {
-        String ipt = Console.readLine();
-        try {
-            int date = Integer.parseInt(ipt);
-            if (date < 1 || date > 31) {
-                throw new IllegalArgumentException(Error.DATE_WRONG_NUMBER.getMessage());
-            }
-            return date;
-        } catch (NumberFormatException e) {
-            throw new NumberFormatException(Error.DATE_NOT_A_NUMBER.getMessage());
-        }
-    }
-
-    private int loopDateInput() {
-        while (true) {
-            try {
-                return runDateInput();
-            } catch (NumberFormatException e) {
-                System.out.println(e.getMessage());
-            } catch (IllegalArgumentException e) {
-                System.out.println(e.getMessage());
-            }
-        }
+        int date = dateInput.getDate();
     }
 }
