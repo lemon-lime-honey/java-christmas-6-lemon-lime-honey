7de8bfd2bea00f72c42c21f3ff56bbbb13250d5a
lemon-lime-honey <bs0308@g.skku.edu>
2023-11-14 18:19:52 +0900

    fix: preview message
    - 혜택 미리보기 메시지 수정

--- src/main/java/christmas/userinput/Message.java
+++ src/main/java/christmas/userinput/Message.java
@@ -3,7 +3,7 @@ package christmas.userinput;
 public enum Message {
     INIT("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.\n12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)"),
     ORDER("주문하실 메뉴를 메뉴와 개수를 알려 주새요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)"),
-    PREVIEW("에 우테코 식당에서 받을 이벤트 혜택 미리 보기!"),
+    PREVIEW("12월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!"),
     ORDERED_MENU("<주문 메뉴>"),
     TOTAL_COST_BEFORE("<할인 전 총주문 금액>"),
     GIFT("<증정 메뉴>"),
@@ -19,5 +19,9 @@ public enum Message {
 
     public String getMessage() {
         return this.message;
+    }
+
+    public String getMessage(int date) {
+        return String.format(message, date);
     }
 }
