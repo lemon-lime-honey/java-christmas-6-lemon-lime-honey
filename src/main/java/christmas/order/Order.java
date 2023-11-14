8e6c3762a3f8190c71c5dd51892cf48351d5590f
lemon-lime-honey <bs0308@g.skku.edu>
2023-11-14 22:15:22 +0900

    feat: calculate order amount
    - 할인받지 않은 총 주문 금액 계산 구현
    - 총 주문 금액, 주문 정보 getter 작성

--- src/main/java/christmas/order/Order.java
+++ src/main/java/christmas/order/Order.java
@@ -1,9 +1,11 @@
 package christmas.order;
 
 import java.util.EnumMap;
+import java.util.Map;
 
 public class Order {
-    private EnumMap<Menu, Integer> order = new EnumMap<>(Menu.class);
+    private Map<Menu, Integer> order = new EnumMap<>(Menu.class);
+    private int totalCost;
 
     public void saveOrder(Menu menu, int quantity) {
         if (order.containsKey(menu)) {
@@ -11,5 +13,21 @@ public class Order {
             return;
         }
         order.put(menu, quantity);
+    }
+
+    public void calculate() {
+        this.totalCost = 0;
+
+        for (Map.Entry<Menu, Integer> entry: order.entrySet()) {
+            this.totalCost += entry.getKey().getCost() * entry.getValue();
+        }
+    }
+
+    public int getTotal() {
+        return this.totalCost;
+    }
+
+    public Map<Menu, Integer> getOrder() {
+        return this.order;
     }
 }
