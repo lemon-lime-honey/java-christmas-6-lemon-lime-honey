073958a672f6bdfbd58998e26a30645838035c93
lemon-lime-honey <bs0308@g.skku.edu>
2023-11-14 15:45:13 +0900

    feat: error messages
    - 사용자 입력 오류 시 출력하기 위한 오류 메시지
    - 다음의 경우 오류가 발생한다.
        - 날짜
            - 정수로 변환할 수 없는 경우
            - 1 이상 31 이하가 아닌 경우
        - 메뉴
            - 존재하지 않는 메뉴인 경우
            - 수량이 1 이상이 아닌 경우
            - 형식이 올바르지 않은 경우
            - 중복된 메뉴를 입력한 경우

---
+++ src/main/java/christmas/userinput/Error.java
@@ -0,0 +1,20 @@
+package christmas.userinput;
+
+public enum Error {
+    DATE_NOT_A_NUMBER("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요."),
+    DATE_WRONG_NUMBER("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요."),
+    MENU_NOT_EXIST("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요."),
+    MENU_WRONG_NUMBER("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요."),
+    MENU_WRONG_STYLE("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요."),
+    MENU_DUPLICATE("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+
+    private final String message;
+
+    Error(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return this.message;
+    }
+}
